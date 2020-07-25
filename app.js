bf9c6affa47c633640847ba51ef1ce227444f8f5
vitor lopes <lopesvitor959@gmail.com>
2020-07-25 11:45:31 +0100

    Saturday

--- app.js
+++ app.js
@@ -94,7 +94,8 @@ app.locals.title = 'Express - Generated with IronGenerator';
 app.use(
   cors({
     credentials: true,
-    origin: ['http://localhost:3000']
+    origin: ['http://localhost:3000',
+   'http://proj3fe.s3-website-eu-west-1.amazonaws.com/']
   })
 )
 
